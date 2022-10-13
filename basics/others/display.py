d9c12f920de95a1c92f5dcc4eb0d752c7e59c2d6
Harshdikshit29 <111993546+Harshdikshit29@users.noreply.github.com>
2022-10-13 18:22:19 +0530

    create others.py

---
+++ basics/others/display.py
@@ -0,0 +1,11 @@
+name= 'harsh'
+age= 19
+
+print(name, age)
+print(name + str(age))
+print('my name is', name, 'and i am', age, 'years old')
+print('my name is', name, 'and i am', age, 'years old', sep='👀')
+
+#f-string (important)
+print(f'my name is {name} and i am {age} years old')
+print(f'my name is {name} \n\nand i am {age} years old') #\n works like enter
---
+++ basics/others/info.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+college = input("Where do you study: ")
+city = input("City name: ")
+age = input("Enter your age: ")
+animal = input("National animal of India: ")
+print(name, college, city, age, animal)
+
+age = int(age) #typecasting
+print(age, type(age))
+
+a = int(input('enter a number=> '))
+b = int(input('enter a number=> '))
+print(a +b)
