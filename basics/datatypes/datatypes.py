c79c8aa46d2835bb2799a1f07d18119d15d28090
Harshdikshit29 <111993546+Harshdikshit29@users.noreply.github.com>
2022-10-13 18:07:53 +0530

    Create datatypes.py

---
+++ basics/datatypes/datatypes.py
@@ -0,0 +1,66 @@
+#Integer
+x= 125
+y= -128
+z= 555
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+#Float
+x= 125.05
+y= -12.8
+z= 5.55
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+#String
+name1= 'harsh'
+name2= 'niketa'
+name3= 'python'
+print(name1, type(name1))
+print(name2, type(name2))
+print(name3, type(name3))
+
+#multiline string
+print('''python data science''')
+
+#Boolean
+is_coding= True
+is_fun= True
+are_u_bored= False
+print(is_coding, type(is_coding))
+print(is_fun, type(is_fun))
+print(are_u_bored, type(are_u_bored))
+
+#None
+x= None
+print(x, type(x))
+
+#List
+a=[5,10,15,20,25,30,35,40]
+vals=[1,2.2,'harsh']
+colors=['red','green','blue']
+print(a, type(a))
+print(vals, type(vals))
+print(colors, type(colors))
+
+#Tuples
+a=(5,10,15,20,25,30,35,40)
+vals=(1,2.2,'harsh')
+colors=('red','green','blue')
+print(a, type(a))
+print(vals, type(vals))
+print(colors, type(colors))
+
+#Sets
+a={1,2.2,'harsh','red'}
+print(a, type(a))
+
+#Dictionary
+car_info= {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(car_info, type(car_info))
